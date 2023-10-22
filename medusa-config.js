19e20a9ee197df10ef3ccbba5cafac7f155e3e69
jibril <jazimjibril@gmail.com>
2023-10-22 23:19:31 +0300

    Redis Changes ⚡

--- medusa-config.js
+++ medusa-config.js
@@ -55,7 +55,7 @@ const plugins = [
 ];
 
 const modules = {
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
       redisUrl: REDIS_URL
@@ -66,7 +66,7 @@ const modules = {
     options: {
       redisUrl: REDIS_URL
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
